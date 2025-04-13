c2588cbae8347410248c9bc5cc9f3678928ce230
ChristopherHatesCoding <chrisboi69777@gmail.com>
2025-04-13 01:33:34 -0400

    merge changes

--- calculator.py
+++ calculator.py
@@ -17,3 +17,18 @@ def log(a,b):
         print(str(error))
 def exp(a,b):
     return a**b
+
+import math
+def add(a, b):
+    return a+b
+def subtract(a, b):
+    return a-b
+def multiply(a, b):
+    return a*b
+def logarithm(a,b):
+    try:
+        return math.log(b,a)
+    except ValueError as error:
+        print(str(error))
+def exponent(a,b):
+    return a**b
