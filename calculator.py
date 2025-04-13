85edc7884868455278d5ca070dd10c9886117c4c
ChristopherHatesCoding <chrisboi69777@gmail.com>
2025-04-13 01:23:44 -0400

    Part 1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,19 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
-
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
-
-
+import math
+def add(a, b):
+    return a+b
+def sub(a, b):
+    return a-b
+def mul(a, b):
+    return a*b
+def div(a, b):
+    try:
+        return b/a
+    except ZeroDivisionError as error:
+        print(str(error))
+def log(a,b):
+    try:
+        return math.log(b,a)
+    except ValueError as error:
+        print(str(error))
+def exp(a,b):
+    return a**b
