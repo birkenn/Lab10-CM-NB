2abab6c42088ce60446f33d81fc1c659b3b74dc0
ChristopherHatesCoding <chrisboi69777@gmail.com>
2025-04-13 02:08:41 -0400

    Partner 2 Unit Tests

--- test_calculator.py
+++ test_calculator.py
@@ -3,6 +3,7 @@ import calculator
 
 
 class TestCalculator(unittest.TestCase):
+
     ######### Partner 2
     def test_add(self): # 3 assertions
         self.assertEqual(calculator.add(1, 2), 3)
@@ -14,15 +15,6 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(calculator.subtract(2, 3), 1)
         self.assertEqual(calculator.subtract(7, 8), 0)
 
-    ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
-
-    # def test_divide(self): # 3 assertions
-    #     fill in code
-    # ##########################
-
-    ######## Partner 2
     def test_divide_by_zero(self):
             with self.assertRaises(ZeroDivisionError):
                 calculator.div(0, 10)
@@ -34,27 +26,27 @@ class TestCalculator(unittest.TestCase):
     def test_log_invalid_base(self):
         with self.assertRaises(ZeroDivisionError):
             calculator.logarithm(-1)
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
-    # ##########################
-    
-    ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
-
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
-    ##########################
+    def test_multiply(self):
+        self.assertEqual(calculator.mul(2,3),6)
+        self.assertEqual(calculator.mul(2,4),8)
+        self.assertEqual(calculator.mul(2, 5), 10)
+    def test_divide(self):
+        self.assertEqual(calculator.div(1,5),5)
+        self.assertEqual(calculator.div(2, 10), 5)
+        self.assertEqual(calculator.div(3, 15), 5)
+    def test_log_invalid_argument(self):
+        with self.assertRaises(ValueError):
+            calculator.logarithm(0,10)
+    def test_hypotenuse(self):
+        self.assertEqual(calculator.hypotenuse(3, 4), 5)
+        self.assertEqual(calculator.hypotenuse(6, 8), 10)
+        self.assertEqual(calculator.hypotenuse(5, 12), 13)
+    def test_sqrt(self):
+        with self.assertRaises(ValueError):
+            calculator.square_root(-33)
+        self.assertEqual(calculator.square_root(9), 3)
+        self.assertEqual(calculator.square_root(4), 2)
 
 # Do not touch this
 if __name__ == "__main__":
